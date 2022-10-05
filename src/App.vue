4694b5f27580126afed9b1deb70bbd6e04deee07
Ahmed K. A <ahmadkabdullah@pm.me>
2022-10-06 00:11:34 +0300

    feat: set different style and add more structure

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,34 @@
 
 <template>
   <div
-    class="min-w-screen min-h-screen p-4 bg-true-gray-300 text-true-gray-700"
+    class="min-w-screen min-h-screen bg-true-gray-800 text-true-gray-200 cursor-default select-none"
   >
-    <header>
-      <h1 class="text-3xl font-bold">
-        Good
-        <h1 class="text-amber-300">{{ formatted }}</h1>
-      </h1>
+    <!-- header -->
+    <header class="grid md:(grid-cols-2) p-8 h-screen">
+      <!-- name -->
+      <div class="p-8 flex flex-col gap-8 items-start justify-center">
+        <h1 class="text-5xl font-bold">I am Ahmed</h1>
+        <p class="text-3xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nam
+          repudiandae nemo.
+        </p>
+      </div>
+      <!-- name end -->
+      <!-- date -->
+      <div class="flex flex-col md:(justify-start) items-end justify-center">
+        <h1 class="text-6xl font-bold">Good</h1>
+        <h1 class="text-4xl font-bold text-amber-200">
+          {{ formatted }}
+        </h1>
+      </div>
+      <!-- date end -->
     </header>
+    <!-- header end -->
+    <!-- main -->
+    <main class="h-screen p-8">
+      <h1>Hello</h1>
+      <h2>This is new</h2>
+    </main>
+    <!-- main end -->
   </div>
 </template>
